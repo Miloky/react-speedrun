f50f78ec09600d80a185512e5f0797941b880b44
P1xt <kjphpdev@gmail.com>
2017-09-06 17:17:09 -0500

    Mock out random quote machine.

--- client/src/containers/frontend/quote/quote.js
+++ client/src/containers/frontend/quote/quote.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Card, CardActions, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import './quote.css';
 
 const Quote = props => (
-  <div>
-    <h1>Quote</h1>
+  <div className="quote">
+    <header className="quote__header">
+      <h1>Random Quote Machine</h1>
+    </header>
+    <section className="quote__display">
+      <Card className="quote__card">
+        <CardText>
+          <div className="quote__text">{props.quote.text}</div>
+          <div className="quote__author">{props.quote.author}</div>
+        </CardText>
+        <CardActions>
+          <FlatButton label="Tweet" />
+          <FlatButton label="Next" />
+        </CardActions>
+      </Card>
+    </section>
+
   </div>
-  
+
 )
 
 const mapStateToProps = state => ({
+  quote: { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                author: "some author"}
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
