c4722d4ae37dd8939238958e0f43910eb1be3221
P1xt <kjphpdev@gmail.com>
2017-09-06 12:46:48 -0500

    Create initial styles for the frontend.
    Style Header, Footer, and Sidebar.
    Link to Github in Footer.

--- client/src/containers/home/index.js
+++ client/src/containers/home/index.js
@@ -7,16 +7,14 @@ import RaisedButton from 'material-ui/RaisedButton';
 const Home = props => (
   <div>
     <h1>Home</h1>
-
-    <p><RaisedButton onClick={() => props.changePage()} label="Go to about page via redux" /></p>
   </div>
+  
 )
 
 const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  changePage: () => push('/about-us')
 }, dispatch)
 
 export default connect(
--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,12 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
-import App from './containers/app';
+import App from './containers/app/app.jsx';
 import './index.css';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import {blueGrey600, blueGrey400, blueGrey800, orange800, blueGrey100, grey700, darkBlack, white, grey300, fullBlack} from 'material-ui/styles/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // Needed for onTouchTap
@@ -14,11 +17,31 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 injectTapEventPlugin();
 
 const target = document.querySelector('#root')
-
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: blueGrey600,
+    primary2Color: blueGrey400,
+    primary3Color: blueGrey800,
+    accent1Color: orange800,
+    accent2Color: blueGrey100,
+    accent3Color: grey700,
+    textColor: darkBlack,
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: fade(darkBlack, 0.3),
+    pickerHeaderColor: blueGrey600,
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack,
+  },
+  appBar: {
+    height: 50,
+  },
+});
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
         <div>
           <App />
         </div>
