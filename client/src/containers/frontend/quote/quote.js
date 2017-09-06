921554223ad0ad02ff25b063ac4853e0a745ba40
P1xt <kjphpdev@gmail.com>
2017-09-06 18:35:53 -0500

    Retrieve quotes for random quote app.

--- client/src/containers/frontend/quote/quote.js
+++ client/src/containers/frontend/quote/quote.js
@@ -1,41 +1,55 @@
-import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
 import { Card, CardActions, CardText } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import './quote.css';
 
-const Quote = props => (
-  <div className="quote">
-    <header className="quote__header">
-      <h1>Random Quote Machine</h1>
-    </header>
-    <section className="quote__display">
-      <Card className="quote__card">
-        <CardText>
-          <div className="quote__text">{props.quote.text}</div>
-          <div className="quote__author">{props.quote.author}</div>
-        </CardText>
-        <CardActions>
-          <FlatButton label="Tweet" />
-          <FlatButton label="Next" />
-        </CardActions>
-      </Card>
-    </section>
+class Quote extends Component {
+  state = {
+    source: "https://quotes.rest/qod.json?category=",
+    quote: { text: '', author: ''}
+    }
+  async getQuote() {
+    var catagories = ['inspire', 'funny', 'students', 'life', 'management', 'sports'];
+    var randomCategory = Math.floor((Math.random() * (catagories.length - 1) + 0));
 
-  </div>
+    const response = await fetch(this.state.source + catagories[randomCategory]);
+    const data = await response.json();;
+    this.setState({
+      quote: { 
+        text: data.contents.quotes[0].quote,
+        author: data.contents.quotes[0].author
+      }
+    })
+  }
+  async componentDidMount() {
+    this.getQuote();
+  }
+  render() {
+    return (
+      <div className="quote">
+        <header className="quote__header">
+          <h1>Random Quote Machine</h1>
+        </header>
+        <section className="quote__display">
+          <Card className="quote__card">
+            <CardText className="quote__card__text">
+              <div className="quote__text">{this.state.quote.text}</div>
+              <div className="quote__author">Author - {this.state.quote.author}</div>
+            </CardText>
+            <CardActions className="quote__card__actions">
+              <RaisedButton label="Tweet" primary={true} />
+              <RaisedButton onClick={e => {
+                e.preventDefault();
+                this.getQuote();
+              }} label="Next" secondary={true}/>
+            </CardActions>
+          </Card>
+        </section>
 
-)
+      </div>
+    )
+  }
+}
 
-const mapStateToProps = state => ({
-  quote: { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                author: "some author"}
-})
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-}, dispatch)
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Quote)
+export default Quote;
