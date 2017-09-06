fd7025ac74fea46069d52479d5b184ec54c60f61
P1xt <kjphpdev@gmail.com>
2017-09-05 20:55:26 -0500

    Scaffold server app

---
+++ server/server.js
@@ -0,0 +1,28 @@
+
+import bodyParser from 'body-parser';
+import express from 'express';
+import path from 'path';
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const router = express.Router();
+
+const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+app.use(staticFiles);
+
+router.get('/api', (req, res) => {
+  console.log('Retrieved data from API');
+  res.json('Retrieved data from API');
+})
+
+app.use(router);
+
+// any routes not picked up by the server api will be handled by the react router
+app.use('/*', staticFiles);
+
+app.set('port', (process.env.PORT || 3001));
+app.listen(app.get('port'), () => {
+  console.log(`Listening on ${app.get('port')}`);
+});
