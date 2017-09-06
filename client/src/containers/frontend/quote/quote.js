28a5b620c98b49eb6a4520fb4a15019d516fb313
P1xt <kjphpdev@gmail.com>
2017-09-06 18:43:25 -0500

    Chore: tidy quote generator

--- client/src/containers/frontend/quote/quote.js
+++ client/src/containers/frontend/quote/quote.js
@@ -5,15 +5,15 @@ import './quote.css';
 
 class Quote extends Component {
   state = {
-    source: "https://quotes.rest/qod.json?category=",
+    source: 'https://quotes.rest/qod.json?category=',
     quote: { text: '', author: ''}
     }
   async getQuote() {
-    var catagories = ['inspire', 'funny', 'students', 'life', 'management', 'sports'];
-    var randomCategory = Math.floor((Math.random() * (catagories.length - 1) + 0));
+    const catagories = ['inspire', 'funny', 'students', 'life', 'management', 'sports'];
+    const randomCategory = Math.floor((Math.random() * (catagories.length - 1) + 0));
 
     const response = await fetch(this.state.source + catagories[randomCategory]);
-    const data = await response.json();;
+    const data = await response.json();
     this.setState({
       quote: { 
         text: data.contents.quotes[0].quote,
@@ -26,22 +26,22 @@ class Quote extends Component {
   }
   render() {
     return (
-      <div className="quote">
-        <header className="quote__header">
+      <div className='quote'>
+        <header className='quote__header'>
           <h1>Random Quote Machine</h1>
         </header>
-        <section className="quote__display">
-          <Card className="quote__card">
-            <CardText className="quote__card__text">
-              <div className="quote__text">{this.state.quote.text}</div>
-              <div className="quote__author">Author - {this.state.quote.author}</div>
+        <section className='quote__display'>
+          <Card className='quote__card'>
+            <CardText className='quote__card__text'>
+              <div className='quote__text'>{this.state.quote.text}</div>
+              <div className='quote__author'>Author - {this.state.quote.author}</div>
             </CardText>
-            <CardActions className="quote__card__actions">
-              <RaisedButton label="Tweet" primary={true} />
+            <CardActions className='quote__card__actions'>
+              <RaisedButton label='Tweet' primary={true} />
               <RaisedButton onClick={e => {
                 e.preventDefault();
                 this.getQuote();
-              }} label="Next" secondary={true}/>
+              }} label='Next' secondary={true}/>
             </CardActions>
           </Card>
         </section>
